4f1b9b999a894a34bde0a192ff809cc996900151
y0n1k <yan.pekarski@gmail.com>
2025-02-15 13:19:44 +0200

    push test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+      Hello!
       <main className={styles.main}>
         <Image
           className={styles.logo}
