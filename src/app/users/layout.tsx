93fc14587c3e0824f2bfd75df79fea8f7a883e4b
y0n1k <yan.pekarski@gmail.com>
2025-02-15 13:47:30 +0200

    git ignore

---
+++ src/app/users/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "UsersLayout metadata"
+}
+
+type Props = {children: React.ReactNode}
+
+const UsersLayout = ({children}:Props) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default UsersLayout;
